69a3de73991de70696d8ac9c1d81d3a0d7bd3807
logonpf <logonpf@fiap.com.br>
2017-08-02 20:59:47 -0300

    .NET Apresentação

---
+++ Fiap.HelloWorld/Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiap.HelloWorld.UI.Model;
+
+namespace Fiap.HelloWorld.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar um carro
+            Carro carro = new Carro();
+            //Setar um modelo e placa
+            carro.Modelo = "Fusca";
+            carro.Placa = "EDS-9000";
+
+            //Imprimir o modelo
+            Console.WriteLine(carro.Modelo);
+            Console.ReadLine();
+        }
+    }
+}
