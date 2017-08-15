b84e54656048335f8d8dc14994721cede7cbd75c
logonpf <logonpf@fiap.com.br>
2017-08-14 22:45:06 -0300

    ASP.NET MVC

---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Controllers/CervejaController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Exemplo02.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Controllers
+{
+    public class CervejaController : Controller
+    {        
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Cerveja cerveja)
+        {
+            //Enviando valores para a view
+            ViewBag.nomeCerveja = cerveja.Nome;
+            ViewBag.data = cerveja.Validade;
+            ViewBag.alcool = cerveja.Teor;
+
+            TempData["msg"] = "Cerveja cadastrada!";
+
+            return View(cerveja);
+            //return Content("Gravou!" + " " + cerveja.Nome + " " + 
+            //    cerveja.Teor + " " + cerveja.Validade);
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Cerveja.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo02.MVC.Models
+{
+    public class Cerveja
+    {
+        public string Nome { get; set; }
+        public DateTime Validade { get; set; }
+        public double Teor { get; set; }
+    }
+}
