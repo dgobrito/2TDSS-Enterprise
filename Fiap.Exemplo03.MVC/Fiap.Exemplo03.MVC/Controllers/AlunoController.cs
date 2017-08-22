c79992a97e77a2ff87e93f6023dde21c14505fbb
logonpf <logonpf@fiap.com.br>
2017-08-21 22:34:58 -0300

    Apostila 04

---
+++ Fiap.Exemplo03.MVC/Fiap.Exemplo03.MVC/Controllers/AlunoController.cs
@@ -0,0 +1,40 @@
+﻿using Fiap.Exemplo03.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo03.MVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        //Simular o banco de dados
+        private static List<Aluno> _banco = new List<Aluno>();
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            _banco.Add(aluno);
+            TempData["msg"] = "Cadastrado com sucesso!";
+            CarregarComboBox();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            CarregarComboBox();
+            return View();
+        }
+
+        private void CarregarComboBox()
+        {
+            List<string> cursos = new List<string>();
+            cursos.Add("ADS");
+            cursos.Add("SI");
+            cursos.Add("DB");
+            ViewBag.opcoes = new SelectList(cursos);
+        }
+    }
+}
---
+++ Fiap.Exemplo03.MVC/Fiap.Exemplo03.MVC/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo03.MVC.Models
+{
+    public class Aluno
+    {
+        public string  Nome { get; set; }
+        public int Rm { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Matriculado { get; set; }
+        public string Curso { get; set; }
+    }
+}
